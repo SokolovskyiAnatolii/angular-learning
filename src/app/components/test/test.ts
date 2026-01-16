6c98b448fcc5c5bcc0a2ae5b98481d75a954ab25
Anatolii Sokolovskyi <sans357@gmail.com>
2026-01-16 16:53:45 +0200

    initial commit: ngular learning project

---
+++ src/app/components/test/test.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-test',
+  imports: [],
+  templateUrl: './test.html',
+  styleUrl: './test.scss',
+})
+export class TestComponent {
+  title = 'Це інтерполяція в TestComponent';
+  fisrstName = 'Анатолій';
+  secondName = 'Соколовський';
+
+  getFullName(): string {
+    return `Мене звати ${this.fisrstName} ${this.secondName}`;
+  }
+
+}
