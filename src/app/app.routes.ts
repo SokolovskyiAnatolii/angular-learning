fbe2d34826037b7b572db1ae53e1719c0e59a400
Anatolii Sokolovskyi <sans357@gmail.com>
2026-01-29 08:22:36 +0200

    feat: define application routes for navigation and component loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', loadComponent: () => component: HomeComponent},
+  { path: 'angular', loadComponent: () => component: HomeComponent },
+  { path: 'typescript', loadComponent: () => component: HomeComponent},
+  { path: 'javascript', loadComponent: () => component: HomeComponent },
+  { path: 'rxjs', loadComponent: () => component: HomeComponent}
+  { path: '**', loadComponent: () => component: PageNotFoundComponent }
+];
