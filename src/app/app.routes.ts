0dc9e7624cad46f6c105ae573fb84a9b93ecbd1a
Sokolovskyi Anatolii <42448977+SokolovskyiAnatolii@users.noreply.github.com>
2026-01-30 17:23:06 +0200

    feat: add Angular components and routing for home, angular, typescript, javascript, rxjs, and page-not-found

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home';
+import { AngularComponent } from './components/angular/angular';
+import { TypescriptComponent } from './components/typescript/typescript';
+import { JavascriptComponent } from './components/javascript/javascript';
+import { RxjsComponent } from './components/rxjs/rxjs';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'angular', component: AngularComponent },
+  { path: 'typescript', component: TypescriptComponent },
+  { path: 'javascript', component: JavascriptComponent },
+  { path: 'rxjs', component: RxjsComponent },
+  { path: '**', component: PageNotFoundComponent }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,44 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, signal, Type } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { TestComponent } from './components/test/test';
 import { TodoComponent } from './components/todo/todo';
 import { FormComponent } from './components/form/form';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatTableModule } from '@angular/material/table';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TestComponent,TodoComponent,FormComponent],
+  imports: [
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive,
+    CommonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    MatListModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatTabsModule,
+    MatTableModule,
+    MatGridListModule,
+    MatButtonModule,
+    MatSidenavModule,
+    TestComponent,
+    TodoComponent,
+    FormComponent
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
